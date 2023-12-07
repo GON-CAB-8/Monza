088a28d289a8eec4586ad199dfab6bf400543f1a
GON-CAB-8 <cabralmartin8@gmail.com>
2023-12-06 23:14:16 -0300

    proyecto base seteado

---
+++ src/main/java/com/proton/monza/MonzaApplication.java
@@ -0,0 +1,13 @@
+package com.proton.monza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proton/monza/MonzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proton.monza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
